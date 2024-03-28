02f57645583fdd22dd961d39abacd5563bd49ea1
Sarunas <sarugen12@gmail.com>
2024-03-28 20:15:14 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int skaiciusA = 8;
+        int skaiciusB = 4;
+
+        double extraSkaicius = 5.51;
+
+//        while (skaiciusA < 100 || skaiciusB < 20)
+//        {
+//            skaiciusA += 5;
+//            skaiciusB += 5;
+//            System.out.println("Skaicius A: " + skaiciusA);
+//            System.out.println("Skaicius B: " + skaiciusB);
+//        }
+
+        /* while (false)
+        {
+            System.out.println(skaiciusA);
+        } */
+
+    }
+}
