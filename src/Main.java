e53c8b1003cdcee69f98c720ac104053f83407ce
Sarunas <sarugen12@gmail.com>
2024-03-28 20:22:58 +0200

    V1

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,7 @@ public class Main {
         int skaiciusA = 8;
         int skaiciusB = 4;
 
-        double extraSkaicius = 5.51;
-
-//        while (skaiciusA < 100 || skaiciusB < 20)
-//        {
-//            skaiciusA += 5;
-//            skaiciusB += 5;
-//            System.out.println("Skaicius A: " + skaiciusA);
-//            System.out.println("Skaicius B: " + skaiciusB);
-//        }
-
-        /* while (false)
-        {
-            System.out.println(skaiciusA);
-        } */
+        double extraSkaicius = 5.50;
 
     }
 }
